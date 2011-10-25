55b60d2cf99d8b2479a37473061dce61496b9637
Nicolas Zuasti <zonic@outland.(none)>
2011-10-25 18:16:43 -0200

    Primer intento de estilo completo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+	#Devuelve el titulo de la pagina
+	def title
+		base_title = "Sistema de gestion integral de laboratorio"
+		
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end
+	end
+	
+	def titulo
+		primerParte = "Gestion integral de laboratorio"
+		
+		if @title.nil?
+			primerParte
+		else
+			"#{primerParte} | #{@title}"
+		end
+	end
 end
