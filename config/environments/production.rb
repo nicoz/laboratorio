1da0d9b2d19ef898a69124e2db25b030228e8c47
Nicolas Zuasti <zonic@outland.(none)>
2011-10-25 21:01:34 -0200

    Arreglado problema de precompilacion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,6 @@ Laboratorio::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.assets.precompile += %w( *.css *.js )
 end
