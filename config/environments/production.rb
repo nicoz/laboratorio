1108bbb03134e5df564897c14b79c11ac12850d6
Nicolas Zuasti <zonic@outland.(none)>
2011-12-28 17:54:58 -0200

    arreglando ui nueva con heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Laboratorio::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #config.assets.compile = false asi estaba antes
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
