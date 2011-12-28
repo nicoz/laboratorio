0f2978080d95251b06ac9e9edfa271b91b9fc3ae
Nicolas Zuasti <zonic@outland.(none)>
2011-12-28 17:26:57 -0200

    finalizada la adaptacion de la nueva interfaz grafica

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,9 @@ class SessionsController < ApplicationController
   def new
   	if usuario_actual.nil?
   		@title = "Ingreso"
+  		flash[:notice] = "Debe ingresar su usuario y clave antes de continuar"
   	else
-  		redirect_back_or escritorio_path(usuario_actual)
+  		redirect_back_or escritorio_path
 	end
   end
   
@@ -18,7 +19,7 @@ class SessionsController < ApplicationController
   		render 'new'
   	else
   		ingresar usuario
-  		redirect_back_or escritorio_path(usuario)
+  		redirect_back_or escritorio_path
   	end
   end
   
