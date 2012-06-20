ff9de34741a96efe6b5a809a01f917b6e9dd75fb
Nicolas Zuasti <zonic@zonic-laptop.(none)>
2012-06-20 18:20:33 -0300

    validacion de fechas en zafras

--- app/models/zafra.rb
+++ app/models/zafra.rb
@@ -4,9 +4,24 @@ class Zafra < ActiveRecord::Base
   validates :dia_inicio, :presence => true, :uniqueness => true
   validates :dia_fin, :uniqueness => true
 
+  validate :dia_inicial_usado
+  validate :dia_fin_usado
+
   scope :activa, where('dia_fin = ?', nil)
 
   def to_s
     "Zafra: l #{self.dia_inicio}"
   end
+
+  private
+    def dia_inicial_usado
+      zafras = Zafra.where("dia_inicio <= ? and dia_fin >= ? and id != ?",self.dia_inicio, self.dia_inicio, self.id).count
+      errors.add(:dia_inicio, "La fecha esta incluida dentro de otra zafra") if zafras > 0
+    end
+
+    def dia_fin_usado
+      zafras = Zafra.where("dia_inicio <= ? and dia_fin >= ? and id != ?",self.dia_fin, self.dia_fin, self.id).count
+      errors.add(:dia_fin, "La fecha esta incluida dentro de otra zafra") if zafras > 0
+    end
+
 end
