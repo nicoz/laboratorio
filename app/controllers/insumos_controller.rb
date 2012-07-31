9effe8d6c0b05bc43ddaa8698b0d87e05af8ea97
Nicolas Zuasti <zonic@zonic-laptop.(none)>
2012-07-31 18:45:34 -0300

    arreglo en insumos

--- app/controllers/insumos_controller.rb
+++ app/controllers/insumos_controller.rb
@@ -2,7 +2,7 @@ class InsumosController < ApplicationController
 	before_filter :autenticar, 		:only => [:index, :edit, :update, :destroy, :create, :new, :show]
 	before_filter :validar_turno_dia,	:only => [:new, :edit]
 	add_breadcrumb 'Escritorio', '/escritorio'
-	
+
 	def index
 		add_breadcrumb 'Insumos', turnos_path
 		@title = "Insumos"
@@ -10,25 +10,25 @@ class InsumosController < ApplicationController
 		@insumos = @search.paginate(:page => params[:page], :per_page => 10, :order => 'created_at')
 		@busqueda = params[:search]
 	end
-	
+
 	def show
 		@insumo = Insumo.find(params[:id])
 		@title = "#{@insumo.turno_dia.turno.nombre} #{l @insumo.turno_dia.dia.fecha}"
 		add_breadcrumb 'Insumos', turnos_path
 		add_breadcrumb @insumo.turno_dia.turno.nombre, @insumo
 	end
-	
+
 	def new
 		fecha = Date.parse(params[:fecha])
 		@dia = Dia.find_by_fecha(fecha)
 		if @dia.nil?
 			@dia = Dia.new(:fecha => fecha)
 		end
-		
+
 		add_breadcrumb "#{l @dia.fecha}", ver_dia_path(@dia.fecha, @dia.fecha)
-		
+
 		turnos = Turno.find(:all, :order => 'orden', :conditions => ["habilitado =?", true])
-		
+
 		#creo los turnos para el dia
 		if @dia.turnos.empty?
 			turnos.each do |turno|
@@ -46,36 +46,34 @@ class InsumosController < ApplicationController
 		else
 		        @title = "Editar Insumos"
 		        @dia.turnos.each do |turnoDia|
-		                turnos.delete(turnoDia.turno)		                
+		                turnos.delete(turnoDia.turno)
 			        if turnoDia.insumo.nil?
 				        insumo = Insumo.new
 				        insumo.turnoDia = turnoDia
-				        turnoDia.insumo = insumo				        
+				        turnoDia.insumo = insumo
 			        end
 		        end
-                        
+
                         turnos.each do |turno|
-				td = TurnoDia.new
-				td.turno = turno
-				td.dia = @dia
+				td = TurnoDia.find_or_create_by_dia_id_and_turno_id(@dia.id, turno.id)
+
 				#creo un insumo para cada turno
-				insumo = Insumo.new
-				insumo.turnoDia = td
-				td.insumo = insumo
+				insumo = Insumo.find_or_create_by_turnoDia_id(td)
 				@dia.turnos << td
 			end
                         @dia.turnos.sort! { |a,b| a.turno.orden <=> b.turno.orden }
 			add_breadcrumb 'Editar Insumos', insumos_path
-			
+
 			render 'edit'
 		end
 	end
-	
+
+
 	def create
 		@title = 'Crear Insumos'
-		@dia = Dia.find_or_create_by_fecha(params[:dia][:fecha])
+		@dia = Dia.find_or_create_by_fecha(Date.parse(params[:dia][:fecha]))
 		valido = true
-		turnos = Turno.find(:all, :conditions => ["habilitado =?", true])
+		turnos = Turno.find(:all, :order => 'orden', :conditions => ["habilitado =?", true])
 		turnos.each do |turno|
 			turnoDia = TurnoDia.find_or_create_by_dia_id_and_turno_id(@dia, turno.id)
 			turnoDia.turno = turno
@@ -83,28 +81,29 @@ class InsumosController < ApplicationController
 			insumo.crudoProcesado = params[:turno][turno.id.to_s][:crudoProcesado]
 			insumo.tiraje = params[:turno][turno.id.to_s][:tiraje]
 			insumo.turnoDia = turnoDia
-			
+
 			if insumo.save
 				turnoDia.insumo = insumo
 				@dia.turnos << turnoDia
 			else
 			        valido = false
-				
+
 				turnoDia.insumo = insumo
 				@dia.turnos << turnoDia
-				
+
 			end
 		end
-		
+
 		if valido
 		        flash[:success] = 'Insumos guardados'
 		        redirect_to ver_dia_path(@dia.fecha)
 		else
 		        flash[:error] = 'Errores al guardar el insumo'
+		        @dia.turnos.sort! { |a,b| a.turno.orden <=> b.turno.orden }
 		        render 'new'
 		end
 	end
-	
+
 	def edit
 		turno = TurnoDia.find(params[:turno])
 		add_breadcrumb "#{turno.turno.nombre} #{l turno.dia.fecha}", ver_turno_dia_path(turno.dia.fecha, turno.turno.nombre)
@@ -112,7 +111,7 @@ class InsumosController < ApplicationController
 		@title = "Editar Insumo"
 		add_breadcrumb 'Editar Insumo', insumos_path
 	end
-	
+
 	def update
 		@insumo = Insumo.find(params[:id])
 		turno = TurnoDia.find(params[:insumo][:turnoDia_id])
@@ -124,15 +123,15 @@ class InsumosController < ApplicationController
 			render 'edit'
 		end
 	end
-	
+
 	def destroy
 		#se destruyen?
 	end
-	
+
 	def validar
 		insumo = Insumo.find(params[:insumo]) unless params[:insumo] == '0'
 		insumo = Insumo.new if insumo.nil?
-		
+
 		insumo[params[:nombre]] = params[:valor]
 		insumo.turnoDia_id = params[:turno]
 		if insumo.valid?
@@ -140,8 +139,8 @@ class InsumosController < ApplicationController
 		else
 			mensaje = insumo.errors[params[:nombre]]
 		end
-		
+
 		render :json => {:mensaje => mensaje}
 	end
-	
+
 end
--- app/controllers/produccions_controller.rb
+++ app/controllers/produccions_controller.rb
@@ -2,7 +2,7 @@ class ProduccionsController < ApplicationController
 	before_filter :autenticar, 		:only => [:index, :edit, :update, :destroy, :create, :new, :show]
 	before_filter :validar_turno_dia,	:only => [:new, :edit]
 	add_breadcrumb 'Escritorio', '/escritorio'
-	
+
 	def index
 		add_breadcrumb 'Producciones', turnos_path
 		@title = "Producciones"
@@ -10,27 +10,27 @@ class ProduccionsController < ApplicationController
 		@producciones = @search.paginate(:page => params[:page], :per_page => 10, :order => 'created_at')
 		@busqueda = params[:search]
 	end
-	
+
 	def show
 		@produccion = Produccion.find(params[:id])
 		@title = "#{@produccion.turno_dia.turno.nombre} #{l @produccion.turno_dia.dia.fecha}"
 		add_breadcrumb 'Producciones', turnos_path
 		add_breadcrumb @produccion.turno_dia.turno.nombre, @produccion
 	end
-	
+
 	def new
 		fecha = Date.parse(params[:fecha])
 		@dia = Dia.find_by_fecha(fecha)
 		if @dia.nil?
 			@dia = Dia.new(:fecha => fecha)
 		end
-		
+
 		add_breadcrumb "#{l @dia.fecha}", ver_dia_path(@dia.fecha, @dia.fecha)
-		
+
 		turnos = Turno.find(:all, :order => 'orden', :conditions => ["habilitado =?", true])
-		
+
 		clientes = Cliente.find(:all, :order => 'orden', :conditions => ["habilitado =?", true])
-		
+
 		#creo los turnos para el dia
 		if @dia.turnos.empty?
 			turnos.each do |turno|
@@ -53,7 +53,7 @@ class ProduccionsController < ApplicationController
 			add_breadcrumb 'Crear Produccion', produccions_path
 		else
 			@dia.turnos.each do |turno|
-			        turnos.delete(turno.turno)		                
+			        turnos.delete(turno.turno)
 				if turno.produccion.nil?
 					produccion = Produccion.new
 					produccion.turnoDia = turno
@@ -66,7 +66,7 @@ class ProduccionsController < ApplicationController
 					turno.produccion = produccion
 				end
 			end
-			
+
 			turnos.each do |turno|
 				td = TurnoDia.new
 				td.turno = turno
@@ -89,7 +89,7 @@ class ProduccionsController < ApplicationController
 			render 'edit'
 		end
 	end
-	
+
 	def create
 		@title = 'Crear Produccion'
 		@dia = Dia.find_or_create_by_fecha(params[:dia][:fecha])
@@ -109,7 +109,7 @@ class ProduccionsController < ApplicationController
 			produccion.bigBagAzucarlito = params[:turno][turno.id.to_s][:bigBagAzucarlito]
 			produccion.bigBagDnd = params[:turno][turno.id.to_s][:bigBagDnd]
 			produccion.turnoDia = turnoDia
-			
+
 			clientes.each do |cliente|
 				cliente_produccion = ClienteProduccion.find_or_create_by_cliente_id_and_produccion_id(cliente.id, produccion.id)
 				cliente_produccion.azucar_big_bag = params[:turno][turno.id.to_s][:cliente][cliente.id.to_s][:azucar_big_bag]
@@ -120,7 +120,7 @@ class ProduccionsController < ApplicationController
 					flash[:error] = 'Error al procesar datos del cliente'
 				end
 			end
-			
+
 			if produccion.save
 				turnoDia.produccion = produccion
 				@dia.turnos << turnoDia
@@ -129,16 +129,17 @@ class ProduccionsController < ApplicationController
 				valido = false
 			end
 		end
-		
+
 		if valido
 		        flash[:success] = 'Producciones guardadas'
 		        redirect_to ver_dia_path(@dia.fecha)
 		else
 		        flash[:error] = 'Errores al guardar la produccion'
+		        @dia.turnos.sort! { |a,b| a.turno.orden <=> b.turno.orden }
 		        render 'new'
 		end
 	end
-	
+
 	def edit
 		turno = TurnoDia.find(params[:turno])
 		add_breadcrumb "#{turno.turno.nombre} #{l turno.dia.fecha}", ver_turno_dia_path(turno.dia.fecha, turno.turno.nombre)
@@ -146,7 +147,7 @@ class ProduccionsController < ApplicationController
 		@title = "Editar Produccion"
 		add_breadcrumb 'Editar Produccion', producciones_path
 	end
-	
+
 	def update
 		@produccion = Produccion.find(params[:id])
 		turno = TurnoDia.find(params[:produccion][:turnoDia_id])
@@ -158,15 +159,15 @@ class ProduccionsController < ApplicationController
 			render 'edit'
 		end
 	end
-	
+
 	def destroy
 		#se destruyen?
 	end
-	
+
 	def validar
 		produccion = Produccion.find(params[:produccion]) unless params[:produccion] == '0'
 		produccion = Produccion.new if produccion.nil?
-		
+
 		produccion[params[:nombre]] = params[:valor]
 		produccion.turnoDia_id = params[:turno]
 		if produccion.valid?
@@ -174,7 +175,7 @@ class ProduccionsController < ApplicationController
 		else
 			mensaje = produccion.errors[params[:nombre]]
 		end
-		
+
 		render :json => {:mensaje => mensaje}
 	end
 end
